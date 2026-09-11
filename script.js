0e9c12e7a7a2e9b6706c7a859f76eadcf6e44ade
angelalita77 <anjeanette@gmail.com>
2026-09-11 12:51:00 -0400

    Fixed the three hamburger lines to appear. Removed 'click here' since I can see the lines

---
+++ script.js
@@ -0,0 +1,3 @@
+const menuBtn = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+menuBtn.addEventListener('click', () =>{ navLinks.classList.toggle('open')})
